1a2ad9d08b4033a9b41c841561daa592178d3850
Aeron <ajlaffere@gmail.com>
2024-05-03 02:48:43 +0100

    basic page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import mockup from '$lib/assets/mockup.png';
+</script>
+
+<div class="py-2 px-6">
+<h1 class="text-6xl py-12 font-semibold">We are building a new centre for early-career scientists and technologists to do ambitious work in Central London.</h1>
+
+<div class="grid grid-cols-2 space-x-6">
+	<div>
+
+			<p class="font-semibold">We want more exploratory scientific and technological work to happen in the UK.</p>
+            
+            <p>However, the
+			social and cultural environment isn’t conducive to exceptionally talented, early-career
+			scientists and technologists doing this.</p>
+		<p class="my-6">
+			We’re building a space for technical work in Central London that
+			fixes the problem.
+		</p>
+
+        <div class="my-6">
+		<p class="font-semibold my-2">The new workspace will give the most-exceptional young scientists/technologists:</p>
+		<ul class="list-disc list-inside space-y-2">
+			<li>
+				Access to a peer group; which raises ambition, provides examples of good taste, encourages,
+				and has high expectations.
+			</li>
+			<li>
+				A set of values; pro-technology, pro-progress, optimism, curiosity, non-conformity,
+				ambitiousness.
+			</li>
+			<li>
+				Tools; for laboratory science and manufacturing outside institutional constraints (e.g.
+				university IP-constraints and the permission of supervisors)
+			</li>
+			<li>Access to private knowledge; of the frontier of fields and new opportunities.</li>
+		</ul>
+    </div>
+
+		<p>
+			We will judge our success on whether the workspaces’ members have more ideas, enter new
+			collaborations, and start companies around their work that they otherwise wouldn’t have.
+		</p>
+	</div>
+
+	<div>
+		<img alt="Artist's impression of the space" src={mockup} />
+	</div>
+</div>
+</div>
