e288f6753a7b1ed246d33f2b12fac8d40541c320
Aeron <ajlaffere@gmail.com>
2024-05-03 03:58:56 +0100

    general visual updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,43 +14,49 @@
 	});
 </script>
 
-<div class="px-6 py-2">
-	{#if visible}
-    <div class="text-sm flex justify-between mt-2 mb-4">
-    <p class="font-semibold">London Jackspace</p>
-    <div class="flex">
-        <a href="">Mission</a>
-    </div>
-    </div>
-    <hr />
-		<h1 in:fly={{ x: -400, duration: 1000 }} class="my-12 md:text-6xl text-2xl font-semibold">
-			We are building a new centre for early-career scientists and technologists to do ambitious
-			work in Central London.
-		</h1>
+{#if visible}
+	<div class="font-geist">
+		<div class="bg-emerald-800 py-2">
+			<div class="mb-4 flex justify-between px-4 text-sm text-emerald-50">
+				<p class="font-semibold">London Jackspace</p>
+				<div class="flex gap-x-4">
+					<a href="">Mission</a>
+					<a href="">Team</a>
+				</div>
+			</div>
+			<hr />
+			<h1
+				in:fly={{ x: -400, duration: 1000 }}
+				class="my-12 px-4 text-2xl text-emerald-50 md:text-6xl"
+			>
+				We are building a new centre for early-career scientists and technologists to do ambitious
+				work in Central London.
+			</h1>
+		</div>
 
-		<Carousel.Root class="my-6">
-			<Carousel.Content>
-				<Carousel.Item class="basis-1/3">
-                    <img alt="Artist's impression of the space 1" src={mockup} />
-                </Carousel.Item>
-				<Carousel.Item class="basis-1/3">
-					<img alt="Artist's impression of the space 2" src={mockuptwo} />
-				</Carousel.Item>
-				<Carousel.Item class="basis-1/3">
-                    <img alt="Artist's impression of the space 3" src={mockupthree} /> 
-                </Carousel.Item>
-			</Carousel.Content>
-		</Carousel.Root>
+		<div class="flex-cols-3 mx-4 my-12 flex justify-center space-x-6">
+			<div in:fade={{ duration: 1000, delay: 300 }}>
+				<img alt="Artist's impression of the space 1" src={mockup} />
+			</div>
+			<div in:fade={{ duration: 1000, delay: 600 }}>
+				<img alt="Artist's impression of the space 2" src={mockuptwo} />
+			</div>
+			<div in:fade={{ duration: 1000, delay: 900 }}>
+				<img alt="Artist's impression of the space 3" src={mockupthree} />
+			</div>
+		</div>
 
 		<div>
-			<p class="font-semibold">
-				We want more exploratory scientific and technological work to happen in the UK.
-			</p>
+			<div class="border bg-amber-600 text-amber-50 px-4 py-6">
+				<p class="md:text-6xl text-2xl mb-6">
+					We want more exploratory scientific and technological work to happen in the UK.
+				</p>
 
-			<p>
-				However, the social and cultural environment isn’t conducive to exceptionally talented,
-				early-career scientists and technologists doing this.
-			</p>
+				<p>
+					The present social and cultural environment in the UK isn’t conducive to exceptionally
+					talented, early-career scientists and technologists embarking on ambitious projects.
+				</p>
+			</div>
 			<p class="my-6">
 				We’re building a space for technical work in Central London that fixes the problem.
 			</p>
@@ -81,5 +87,5 @@
 				collaborations, and start companies around their work that they otherwise wouldn’t have.
 			</p>
 		</div>
-	{/if}
-</div>
+	</div>
+{/if}
