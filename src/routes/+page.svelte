f45c0353f2b7f5a48ed572df7397544546142a61
Aeron <ajlaffere@gmail.com>
2024-05-03 17:06:10 +0100

    additional bottom margin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,12 @@
 <svelte:head>
     <title>Jackspace</title>
 </svelte:head>
-<div class="font-geist  ">
+<div class="font-geist">
     <h1 class="md:text-4xl text-3xl mx-6 pt-12 md:w-2/3">We are building a new centre for early-career scientists and technologists to do ambitious work in Central London.</h1>
     <p class="my-6 mx-6 md:w-2/3 md:text-xl text-large">The present social and cultural environment in the UK isn’t conducive to exceptionally talented, early-career scientists and technologists embarking on ambitious projects.</p>
     <p class="my-6 mx-6 md:w-2/3 md:text-xl text-large">We’re building a space for technical work in Central London that fixes the problem.</p>
     <div class="flex flex-col">
     <a href="https://docs.google.com/forms/d/e/1FAIpQLSeAlNNQLhd5X4_SK5PU__lWW3ugq5Cfy57equ8MCOBqioc8SA/viewform" class="py-2 mx-6 my-4 md:text-xl text-large underline underline-offset-4">If you are interested in joining us, please get in touch.</a>
-    <a href="https://docs.google.com/forms/d/e/1FAIpQLSeAlNNQLhd5X4_SK5PU__lWW3ugq5Cfy57equ8MCOBqioc8SA/viewform" class="py-2 mx-6 my-4 md:text-xl text-large underline underline-offset-4">We are inviting interest from funders.</a>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSeAlNNQLhd5X4_SK5PU__lWW3ugq5Cfy57equ8MCOBqioc8SA/viewform" class="py-2 mx-6 mt-4 mb-24 md:text-xl text-large underline underline-offset-4">We are inviting interest from funders.</a>
 </div>
 </div>
