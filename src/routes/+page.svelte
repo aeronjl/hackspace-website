a987a88cfcaa44c1178ba242964886ab1e30d726
Aeron <ajlaffere@gmail.com>
2024-05-07 23:18:35 +0100

    full height

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,19 @@
 <svelte:head>
     <title>Jackspace</title>
 </svelte:head>
-<div class="font-serif font-light flex flex-col mx-auto items-center justify-center h-screen">
+
+<div class="font-serif font-light flex flex-col mx-auto items-center justify-center full-height">
     <div class="flex flex-col gap-y-6 px-6">
     <p class="text-large">A private facility in Central London for the pursuit of the <a href="https://en.m.wikipedia.org/wiki/Useful_art" class="underline">useful arts</a>.</p>
     <p class="text-large">For young scientists and technologists to work without institutional constraint.</p>
     <a href="https://docs.google.com/forms/d/e/1FAIpQLSeAlNNQLhd5X4_SK5PU__lWW3ugq5Cfy57equ8MCOBqioc8SA/viewform" class="text-large underline">Register your interest</a>
 </div>
 </div>
+
+<style>
+    .full-height {
+    height: 100vh; /* Use viewport height directly */
+    /* Fix for mobile Safari viewport bug */
+    height: -webkit-fill-available;
+}
+</style>
