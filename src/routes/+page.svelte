361498d70c978cf35c3ca18a619fc1b856406556
Aeron <ajlaffere@gmail.com>
2024-05-07 23:11:17 +0100

    copy update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,10 @@
 <svelte:head>
     <title>Jackspace</title>
 </svelte:head>
-<div class="font-geist">
-    <h1 class="md:text-4xl text-3xl mx-6 pt-12 xl:w-1/2 md:w-2/3">We are building a new centre for early-career scientists and technologists to do ambitious work in Central London.</h1>
-    <p class="my-6 mx-6 xl:w-1/2 md:w-2/3 md:text-xl text-large">The present social and cultural environment in the UK isn’t conducive to exceptionally talented, early-career scientists and technologists embarking on ambitious projects.</p>
-    <p class="my-6 mx-6 xl:w-1/2 md:w-2/3 md:text-xl text-large">We’re building a space for technical work in Central London that fixes the problem.</p>
-    <div class="flex flex-col">
-    <a href="https://docs.google.com/forms/d/e/1FAIpQLSeAlNNQLhd5X4_SK5PU__lWW3ugq5Cfy57equ8MCOBqioc8SA/viewform" class="py-2 mx-6 my-4 md:text-xl text-large underline underline-offset-4">If you are interested in joining us, please get in touch.</a>
-    <a href="https://docs.google.com/forms/d/e/1FAIpQLSeAlNNQLhd5X4_SK5PU__lWW3ugq5Cfy57equ8MCOBqioc8SA/viewform" class="py-2 mx-6 mt-4 mb-24 md:text-xl text-large underline underline-offset-4">We are inviting interest from funders.</a>
+<div class="font-serif font-light flex flex-col mx-auto items-center justify-center h-screen">
+    <div class="flex flex-col gap-y-6 px-6">
+    <p class="text-large">A private facility in Central London for the pursuit of the <a href="https://en.m.wikipedia.org/wiki/Useful_art" class="underline">useful arts</a>.</p>
+    <p class="text-large">For young scientists and technologists to work without institutional constraint.</p>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSeAlNNQLhd5X4_SK5PU__lWW3ugq5Cfy57equ8MCOBqioc8SA/viewform" class="text-large underline">Register your interest</a>
 </div>
 </div>
