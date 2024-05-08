282ab639ff799df1aee4bf5e63e9335e0bef7712
Aeron <ajlaffere@gmail.com>
2024-05-08 12:39:25 +0100

    form on board

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,57 @@
 <script lang="ts">
+    import { slide } from 'svelte/transition'
+
+    let showForm: boolean = false;
+
+    const toggleForm = () => {
+        showForm = !showForm;
+    }
+
 </script>
 
 <svelte:head>
-    <title>A new space</title>
+	<title>A new space</title>
 </svelte:head>
 
-<div class="font-serif font-light flex flex-col mx-auto items-center justify-center h-screen">
-    <div class="flex flex-col gap-y-6 px-6">
-    <p class="text-large">A private facility in Central London for the pursuit of the <a href="https://en.m.wikipedia.org/wiki/Useful_art" class="underline">useful arts</a>.</p>
-    <p class="text-large">For young scientists and technologists to work without institutional constraint.</p>
-    <a href="https://docs.google.com/forms/d/e/1FAIpQLSeAlNNQLhd5X4_SK5PU__lWW3ugq5Cfy57equ8MCOBqioc8SA/viewform" class="text-large underline">Register your interest</a>
-</div>
+<div class="mx-auto flex h-screen flex-col items-center justify-center font-serif font-light">
+	<div class="flex flex-col gap-y-6 px-6">
+		<p class="">
+			A private facility in Central London for the pursuit of the <a
+				href="https://en.m.wikipedia.org/wiki/Useful_art"
+				class="underline">useful arts</a
+			>.
+		</p>
+		<p class="">
+			For young scientists and technologists to work without institutional constraint.
+		</p>
+		<button
+            on:click={toggleForm}
+			class="underline text-left">
+            Register your interest
+        </button>
+        {#if showForm}
+		<form transition:slide action="/" method="POST">
+			<div class="flex flex-col mb-4">
+				<label for="name" class="mb-1">Name</label>
+				<input type="text" id="name" class="border border-black px-1" placeholder="" />
+			</div>
+			<div class="flex flex-col mb-4">
+				<label for="email" class="mb-1">Email</label>
+				<input type="email" id="email" class="border border-black px-1" placeholder="" />
+			</div>
+			<div class="flex flex-col mb-4">
+				<label for="message" class="mb-1">In 200 words or fewer, describe what you would use the space for</label
+				>
+				<textarea id="message" class="px-1 border border-black" placeholder=""></textarea>
+			</div>
+            <div class="flex flex-col mb-4">
+				<label for="message" class="mb-1">If relevant, please provide the link to an online profile or portfolio of work</label>
+                <input type="text" id="link" class="border border-black px-1" placeholder="" />
+			</div>
+            <button class="underline">Submit</button>
+		</form>
+        {/if}
+	</div>
 </div>
 
 <style>
