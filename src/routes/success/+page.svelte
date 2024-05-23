23add2e8cfa45e2f519dfe1d1b2d8f65b409c66b
Aeron <ajlaffere@gmail.com>
2024-05-23 11:37:16 +0100

    update message on success page

--- src/routes/success/+page.svelte
+++ src/routes/success/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { fade } from 'svelte/transition';
 </script>
-<div in:fade class="mx-auto flex h-screen flex-col max-w-[600px] mt-24 font-serif font-light gap-y-4 text-sm">
-    <p>Thank you, your application has been received.</p>
-    <a href="/" class="underline">Return to home page.</a>
+<div in:fade class="mx-auto flex h-screen flex-col max-w-[400px] mt-24 font-serif font-light gap-y-4 text-sm">
+    <p>Thank you for applying. We are currently raising funds in order to expand our membership and will contact you when we have more information.</p>
+    <a href="/" class="underline">Return to home page</a>
 </div>
